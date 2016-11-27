2ed14b61c660d0756726de4cc12b6cc78bf6bfa3
ProBucket <hasan.khan.pro@gmail.com>
2016-11-27 19:23:35 +0600

    Create php_info.php

---
+++ php_info.php
@@ -0,0 +1,14 @@
+<?php
+
+# Differcence bitween two dates
+$date1 = new DateTime("2010-07-06");
+$date2 = new DateTime("2010-07-09");
+$diff = $date2->diff($date1)->format("%a");
+
+# Date formate change 
+$oldDate = '2010-03-20'
+$arr = explode('-', $oldDate);
+$newDate = $arr[2].'-'.$arr[1].'-'.$arr[0];
+
+# Date formate change 
+$originalDate = "2010-03-21";
