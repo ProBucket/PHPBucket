9bcfe805fb8dda5a527a51476d1891160532cdb9
ProBucket <hasan.khan.pro@gmail.com>
2016-11-28 10:16:13 +0600

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -4,14 +4,30 @@
 $date1 = new DateTime("2010-07-06");
 $date2 = new DateTime("2010-07-09");
 $diff = $date2->diff($date1)->format("%a");
+#end
+
+# Difference bitween two days
+$date1 = "2007-03-24";
+$date2 = "2009-06-26";
+
+$diff = abs(strtotime($date2) - strtotime($date1));
+
+$years = floor($diff / (365*60*60*24));
+$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+
+printf("%d years, %d months, %d days\n", $years, $months, $days);
+#end
 
 # Date formate change 
 $oldDate = '2010-03-20'
 $arr = explode('-', $oldDate);
 $newDate = $arr[2].'-'.$arr[1].'-'.$arr[0];
+#end
 
 # Date formate change 
 $originalDate = "2010-03-21";
 $newDate = date("d-m-Y", strtotime($originalDate));
+#end
 
 
