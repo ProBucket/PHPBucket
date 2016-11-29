ecffc5c5e618a0529b7a9205aa3188dcd5582f4d
ProBucket <hasan.khan.pro@gmail.com>
2016-11-29 09:19:46 +0600

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -1,12 +1,16 @@
 <?php
 
+----------------------------------
 # Differcence bitween two dates
+----------------------------------
 $date1 = new DateTime("2010-07-06");
 $date2 = new DateTime("2010-07-09");
 $diff = $date2->diff($date1)->format("%a");
-#end
+----------------------------------
 
+----------------------------------
 # Difference bitween two days
+----------------------------------
 $date1 = "2007-03-24";
 $date2 = "2009-06-26";
 
@@ -17,7 +21,7 @@ $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
 $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
 
 printf("%d years, %d months, %d days\n", $years, $months, $days);
-#end
+----------------------------------
 
 # Date formate change 
 $oldDate = '2010-03-20'
